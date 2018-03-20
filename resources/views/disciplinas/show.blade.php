d108949ffa5ac5c5bbfaaf933ab7e3156b2680e1
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2018-03-20 09:20:33 -0300

    Listadas as turmas nas páginas de disciplinas.

--- resources/views/disciplinas/show.blade.php
+++ resources/views/disciplinas/show.blade.php
@@ -6,6 +6,17 @@
 
     <p>{{ $disciplina->ementa }}</p>
 
+    <h2>Turmas</h2>
+    
+    <ul>
+    @foreach ($disciplina->turmas as $turma)
+        <li>        
+            {{ $turma->ministrante }}
+            , início em: {{ Carbon\Carbon::parse($turma->inicio)->format('d/m/Y') }}
+        </li>
+    @endforeach
+    </ul>
+
     <input class="btn btn-warning" type="button" onclick='location.href="/disciplinas/{{ $disciplina->id }}/edit";' value="Editar"/>
     &nbsp;
     <input class="btn btn-success" type="button" onclick='location.href="/disciplinas/{{ $disciplina->id }}/turmas/create";' value="Inserir Turma"/>
