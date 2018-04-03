ff3b0dc1f64765bf4ec70104c6cfa9bbc5109e20
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2018-04-03 16:28:05 -0300

    Adicionei os métodos LoginController
    Adicionei o método logout
    Adicionei os campos nomeUnidade e tipoVinculo na migration
    Adicionei a resetrição por admins
    Adicionei a atualização de dados
    Adicionei o redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Socialite;
+use App\User;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -45,7 +47,51 @@ class LoginController extends Controller
 
     public function handleProviderCallback()
     {
-        $user = Socialite::driver('senhaunica')->user();
+        $userSenhaUnica = Socialite::driver('senhaunica')->user();
+        
         // aqui vc pode inserir o usuário no banco de dados local, fazer o login etc.
+
+        # busca o usuário local
+        $user = User::find($userSenhaUnica->id);
+
+        # restrição só para admins
+        $admins = explode(',', trim(env('CODPES_ADMINS')));
+        if (!in_array($userSenhaUnica->id, $admins)) {
+            # exibir mensagem flash de restrição...
+            dd('ACESSO RESTRITO!');
+
+            return redirect('/');
+        }    
+
+        # se o usuário local NÃO EXISTE, cadastra
+        if (is_null($user)) {
+            $user = new User;
+            $user->id = $userSenhaUnica->id;
+            $user->email = $userSenhaUnica->email;
+            $user->name = $userSenhaUnica->name;
+            $user->nomeUnidade = $userSenhaUnica->nomeUnidade;
+            $user->tipoVinculo = $userSenhaUnica->tipoVinculo;
+            $user->save();
+        } else {
+            # se o usuário EXISTE local
+            # atualiza os dados
+            $user->id = $userSenhaUnica->id;
+            $user->email = $userSenhaUnica->email;
+            $user->name = $userSenhaUnica->name;
+            $user->nomeUnidade = $userSenhaUnica->nomeUnidade;
+            $user->tipoVinculo = $userSenhaUnica->tipoVinculo;
+            $user->save(); 
+        }
+
+        # faz login
+        Auth::login($user, true);
+
+        # redireciona
+        return redirect('/');  
+    }
+
+    public function logout() {
+        Auth::logout();
+        return redirect('/');
     }
 }
